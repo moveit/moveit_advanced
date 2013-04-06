0fdef89e8ce5102a9dc8d3a32f1c361e08ab3758
Sachin Chitta <sachinc@zru.willowgarage.com>
2013-04-05 18:58:12 -0700

    computing manipulability over a grid

---
+++ workspace_analysis/src/main.cpp
@@ -0,0 +1,119 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2012, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Sachin Chitta
+*********************************************************************/
+
+#include <moveit/workspace_analysis/workspace_analysis.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  ros::init (argc, argv, "workspace_analysis");
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  /*Get some ROS params */
+  ros::NodeHandle node_handle("~");  
+  double res_x, res_y, res_z;
+  double min_x, min_y, min_z;
+  double max_x, max_y, max_z;
+  
+  if (!node_handle.getParam("min_x", min_x))
+    min_x = 0.0;
+  if (!node_handle.getParam("max_x", max_x))
+    max_x = 0.0;
+  if (!node_handle.getParam("res_x", res_x))
+    res_x = 0.1;
+  
+  if (!node_handle.getParam("min_y", min_y))
+    min_y = 0.0;
+  if (!node_handle.getParam("max_y", max_y))
+    max_y = 0.0;
+  if (!node_handle.getParam("res_y", res_y))
+    res_y = 0.1;
+
+  if (!node_handle.getParam("min_z", min_z))
+    min_z = 0.0;
+  if (!node_handle.getParam("max_z", max_z))
+    max_z = 0.0;
+  if (!node_handle.getParam("res_z", res_z))
+    res_z = 0.1;
+
+  std::string filename;
+  if (!node_handle.getParam("filename", filename))
+    ROS_ERROR("Will not write to file");  
+
+  /* Load the robot model */
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description"); 
+
+  /* Get a shared pointer to the model */
+  robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
+
+  /* Create a robot state*/
+  robot_state::RobotStatePtr robot_state(new robot_state::RobotState(robot_model));
+
+  robot_state::JointStateGroup* joint_state_group = robot_state->getJointStateGroup("arm");  
+
+  /* Construct a planning scene - NOTE: this is for illustration purposes only.
+     The recommended way to construct a planning scene is to use the planning_scene_monitor 
+     to construct it for you.*/
+  planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
+
+  moveit_msgs::WorkspaceParameters workspace;
+  workspace.min_corner.x = min_x;
+  workspace.min_corner.y = min_y;
+  workspace.min_corner.z = min_z;
+  
+  workspace.max_corner.x = max_x;
+  workspace.max_corner.y = max_y;
+  workspace.max_corner.z = max_z;
+
+  /* Load the workspace analysis */
+  workspace_analysis::WorkspaceAnalysis workspace_analysis(planning_scene, true);  
+
+  /* Compute the metrics */
+  std::vector<geometry_msgs::Quaternion> orientations;  
+  workspace_analysis::WorkspaceMetrics metrics = workspace_analysis.computeMetrics(workspace, orientations, joint_state_group, res_x, res_y, res_z);
+  
+  if(!filename.empty())
+    if(!metrics.writeToFile(filename))
+      ROS_INFO("Could not write to file");
+  
+  ros::shutdown();
+  return 0;  
+}
+
---
+++ workspace_analysis/src/workspace_analysis.cpp
@@ -0,0 +1,189 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2012, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Sachin Chitta
+*********************************************************************/
+
+#include <moveit/workspace_analysis/workspace_analysis.h>
+#include <fstream>
+#include <iostream>
+
+namespace workspace_analysis
+{
+
+WorkspaceAnalysis::WorkspaceAnalysis(const planning_scene::PlanningSceneConstPtr &planning_scene,
+                                     bool position_only): planning_scene_(planning_scene), position_only_ik_(position_only)
+{
+  state_validity_callback_fn_ = boost::bind(&WorkspaceAnalysis::isIKSolutionCollisionFree, this, _1, _2);  
+  kinematics_metrics_.reset(new kinematics_metrics::KinematicsMetrics(planning_scene->getCurrentState().getRobotModel()));
+}
+
+bool WorkspaceAnalysis::isIKSolutionCollisionFree(robot_state::JointStateGroup *joint_state_group,
+                                                  const std::vector<double> &ik_solution)
+{
+  joint_state_group->setVariableValues(ik_solution);
+  bool result = !planning_scene_->isStateColliding(*joint_state_group->getRobotState(), joint_state_group->getName());
+  return result;  
+}
+
+std::vector<geometry_msgs::Pose> WorkspaceAnalysis::sampleUniform(const moveit_msgs::WorkspaceParameters &workspace, 
+                                                                  const std::vector<geometry_msgs::Quaternion> &orientations,
+                                                                  const double &x_resolution,
+                                                                  const double &y_resolution,
+                                                                  const double &z_resolution) const
+{
+  std::vector<geometry_msgs::Pose> results;
+  std::vector<geometry_msgs::Quaternion> rotations = orientations;  
+  if(orientations.empty())
+  {
+    geometry_msgs::Quaternion quaternion;
+    quaternion.w = 1.0;    
+    rotations.push_back(quaternion);    
+  }
+  double x_min = workspace.min_corner.x;
+  double y_min = workspace.min_corner.y;
+  double z_min = workspace.min_corner.z;
+
+  unsigned int x_num_points,y_num_points,z_num_points;
+  double x_dim = std::fabs(workspace.min_corner.x - workspace.max_corner.x);
+  double y_dim = std::fabs(workspace.min_corner.y - workspace.max_corner.y);
+  double z_dim = std::fabs(workspace.min_corner.z - workspace.max_corner.z);
+
+  x_num_points = (unsigned int) (x_dim/x_resolution) + 1;
+  y_num_points = (unsigned int) (y_dim/y_resolution) + 1;
+  z_num_points = (unsigned int) (z_dim/z_resolution) + 1;
+
+  ROS_DEBUG("Cache dimension (num grid points) in (x,y,z): %d %d %d",x_num_points,y_num_points,z_num_points);
+  geometry_msgs::Pose pose;  
+  for(std::size_t i=0; i < x_num_points; ++i)
+  {
+    pose.position.x = x_min + i * x_resolution;
+    for(std::size_t j=0; j < y_num_points; ++j)
+    {
+      pose.position.y = y_min + j * y_resolution;
+      for(std::size_t k=0; k < z_num_points; ++k)
+      {
+        pose.position.z = z_min + k * z_resolution;
+        for(std::size_t m=0; m < rotations.size(); ++m)
+        {
+          pose.orientation = rotations[m];
+          //          Eigen::Affine3d pose_eigen;
+          //          tf::poseMsgToEigen(pose, pose_eigen);
+          results.push_back(pose);          
+        }
+      }
+    }
+  }
+  ROS_INFO("Generated %d samples for workspace points",(int) results.size());
+  return results;
+}
+
+workspace_analysis::WorkspaceMetrics WorkspaceAnalysis::computeMetrics(const moveit_msgs::WorkspaceParameters &workspace,
+                                                                       const std::vector<geometry_msgs::Quaternion> &orientations,
+                                                                       robot_state::JointStateGroup *joint_state_group,
+                                                                       const double &x_resolution,
+                                                                       const double &y_resolution,
+                                                                       const double &z_resolution) const
+{
+  workspace_analysis::WorkspaceMetrics metrics;
+  if(!joint_state_group || !planning_scene_)
+  {
+    ROS_ERROR("Joint state group and planning scene should not be null");
+    return metrics;
+  } 
+
+  std::vector<geometry_msgs::Pose> points = sampleUniform(workspace, orientations, x_resolution, y_resolution, z_resolution);
+  //  metrics.joint_values_.resize(metrics.points_.size());
+  //  metrics.manipulability_.resize(metrics.points_.size());
+  metrics.group_name_ = joint_state_group->getName();
+  metrics.robot_name_ = joint_state_group->getRobotState()->getRobotModel()->getName();
+  metrics.frame_id_ =  joint_state_group->getRobotState()->getRobotModel()->getModelFrame();  
+  
+  for(std::size_t i=0; i < points.size(); ++i)
+  {   
+    bool found_ik = joint_state_group->setFromIK(points[i], 1, 0.01, state_validity_callback_fn_);
+    if(found_ik)
+    {
+      ROS_INFO("Found IK: %d", (int) i);      
+      double manipulability_index;      
+      kinematics_metrics_->getManipulabilityIndex(*joint_state_group->getRobotState(), 
+                                                  joint_state_group->getJointModelGroup(),
+                                                  manipulability_index,
+                                                  position_only_ik_);
+      std::vector<double> joint_values;      
+      joint_state_group->getVariableValues(joint_values);
+      metrics.points_.push_back(points[i]);      
+      metrics.joint_values_.push_back(joint_values);      
+      metrics.manipulability_.push_back(manipulability_index);      
+    }
+    if(!ros::ok())
+      return metrics;    
+  }
+  return metrics;  
+}
+
+bool WorkspaceMetrics::writeToFile(const std::string &filename, const std::string &delimiter, bool exclude_strings)
+{
+  logDebug("Writing %d total points to file: %s",(int) points_.size(),filename.c_str());  
+  std::ofstream file;
+  file.open(filename.c_str());
+  if(!file.is_open())
+  {
+    logDebug("Could not open file: %s",filename.c_str());
+    return false;    
+  }
+  if(file.good())
+  {
+    if(!exclude_strings)
+    {
+      file << robot_name_ << std::endl;
+      file << group_name_ << std::endl;
+      file << frame_id_ << std::endl;
+    }    
+    for(std::size_t i=0; i < points_.size(); ++i)
+    {
+      if(joint_values_[i].empty())
+        continue;      
+      file << points_[i].position.x << delimiter << points_[i].position.y << delimiter << points_[i].position.z << delimiter;
+      file << points_[i].orientation.x << delimiter << points_[i].orientation.y  << delimiter << points_[i].orientation.z << delimiter << points_[i].orientation.w << delimiter;
+      for(std::size_t j=0; j < joint_values_[i].size(); ++j)
+        file << joint_values_[i][j] << delimiter;        
+      file << manipulability_[i] << std::endl;      
+    }
+  }  
+  file.close();
+  return true;   
+}
+
+}
