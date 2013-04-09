336908ebcb8157f5d0fa43aa16a861ce8624fa8f
Sachin Chitta <sachinc@willowgarage.com>
2013-04-09 14:23:38 -0700

    fixing licenses

--- moveit_workspace_analysis/include/moveit/workspace_analysis/workspace_analysis.h
+++ moveit_workspace_analysis/include/moveit/workspace_analysis/workspace_analysis.h
@@ -1,23 +1,13 @@
 /*********************************************************************
 *
-* Software License Agreement (BSD License)
+* Software License Agreement (All Rights Reserved)
 *
-*  Copyright (c) 2012, Willow Garage, Inc.
+*  Copyright (c) 2013, Willow Garage, Inc.
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of Willow Garage, Inc. nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
+*  modification is not permitted without the explicit permission of 
+*  Willow Garage Inc.
 *
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
