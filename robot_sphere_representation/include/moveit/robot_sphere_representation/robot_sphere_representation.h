daf38ebfb3c285b37f9687d527825e4b04706fcc
Acorn <acorn@willowgarage.com>
2013-05-21 09:33:41 -0700

    robot_sphere_representation: add simple implementation

--- robot_sphere_representation/include/moveit/robot_sphere_representation/robot_sphere_representation.h
+++ robot_sphere_representation/include/moveit/robot_sphere_representation/robot_sphere_representation.h
@@ -37,11 +37,19 @@
 #ifndef MOVEIT_ROBOT_SPHERE_REPRESENTATION_ROBOT_SPHERE_REPRESENTATION_
 #define MOVEIT_ROBOT_SPHERE_REPRESENTATION_ROBOT_SPHERE_REPRESENTATION_
 
+#include <eigen_stl_containers/eigen_stl_containers.h>
 #include <boost/shared_ptr.hpp>
+#include <map>
 
 namespace robot_model
 {
-  class RobotModel;
+class RobotModel;
+class LinkModel;
+}
+
+namespace srdf
+{
+class Model;
 }
 
 namespace collision_detection
@@ -53,8 +61,33 @@ public:
   RobotSphereRepresentation(boost::shared_ptr<const robot_model::RobotModel> robot_model);
   ~RobotSphereRepresentation();
 
+  // generate spheres for each link. 
+  // Discards old spheres (if any).
+  // An entry will be added to centers_ and radii_ for every link in the model.
+  // The entry will be empty for links with no collision geometry.
+  void genSpheres();
+
+  // read spheres from the srdf
+  // (by default the one associated with RobotModel)
+  void spheresFromSrdf(const srdf::Model *srdf = NULL);
+
+
 private:
+  // generate a single sphere for each link that bounds the entire link.
+  void genBoundingSpheres();
+
+  // Use a single sphere for a link that bounds the entire link.
+  // If there is no collision geometry this creates an empty entry for this
+  // link.
+  void useBoundingSphereForLink(const robot_model::LinkModel& lm);
+
   boost::shared_ptr<const robot_model::RobotModel> robot_model_;
+
+  // Map from link to spheres.
+  // Each link should have an entry.
+  // If entry is empty the link has no collision geometry.
+  std::map<std::string, EigenSTL::vector_Vector3d> centers_;
+  std::map<std::string, std::vector<double> > radii_;
 };
 
 }
--- robot_sphere_representation/src/robot_sphere_representation.cpp
+++ robot_sphere_representation/src/robot_sphere_representation.cpp
@@ -37,11 +37,94 @@
 #include <moveit/robot_sphere_representation/robot_sphere_representation.h>
 #include <moveit/robot_model/robot_model.h>
 
+#include <geometric_shapes/shape_operations.h>
+
+#include <vector>
+
 collision_detection::RobotSphereRepresentation::RobotSphereRepresentation(
     boost::shared_ptr<const robot_model::RobotModel> robot_model)
   : robot_model_(robot_model)
 {
+}
 
+void collision_detection::RobotSphereRepresentation::spheresFromSrdf(const srdf::Model *srdf)
+{
+  if (!srdf)
+    srdf = robot_model_->getSRDF().get();
+
+  centers_.clear();
+  radii_.clear();
+
+  for (std::vector<const robot_model::LinkModel*>::const_iterator lm = robot_model_->getLinkModels().begin() ;
+       lm != robot_model_->getLinkModels().end() ;
+       ++lm)
+  {
+    for (std::vector<srdf::Model::LinkSpheres>::const_iterator lsp = srdf->getLinkSphereApproximations().begin() ;; ++lsp)
+    {
+      const std::string& link_name = (*lm)->getName();
+
+      // if no spheres in srdf, use a single bounding sphere
+      if (lsp == srdf->getLinkSphereApproximations().end())
+      {
+        useBoundingSphereForLink(**lm);
+        break;
+      }
+
+      // use the spheres from srdf
+      if (lsp->link_ == link_name)
+      {
+        for (std::vector<srdf::Model::Sphere>::const_iterator sp = lsp->spheres_.begin() ;
+             sp != lsp->spheres_.end() ;
+             ++sp)
+        {
+          if (sp->radius_ > std::numeric_limits<double>::min())
+          {
+            Eigen::Vector3d center(sp->center_x_,
+                                   sp->center_y_,
+                                   sp->center_z_);
+            centers_[link_name].push_back(center);
+            radii_[link_name].push_back(sp->radius_);
+          }
+        }
+        break;
+      }
+    }
+  }
+}
+
+void collision_detection::RobotSphereRepresentation::genSpheres()
+{
+  genBoundingSpheres();
+}
+
+void collision_detection::RobotSphereRepresentation::genBoundingSpheres()
+{
+  centers_.clear();
+  radii_.clear();
+
+  for (std::vector<const robot_model::LinkModel*>::const_iterator lm = robot_model_->getLinkModels().begin() ;
+       lm != robot_model_->getLinkModels().end() ;
+       ++lm)
+    useBoundingSphereForLink(**lm);
+}
+
+void collision_detection::RobotSphereRepresentation::useBoundingSphereForLink(const robot_model::LinkModel& lm)
+{
+  centers_[lm.getName()].clear();
+  radii_[lm.getName()].clear();
+
+  const shapes::ShapeConstPtr& shape = lm.getShape();
+  if (!shape)
+    return;
+
+  double radius;
+  Eigen::Vector3d center;
+  shapes::computeShapeBoundingSphere(shape.get(), center, radius);
+  if (radius > std::numeric_limits<double>::min())
+  {
+    centers_[lm.getName()].push_back(center);
+    radii_[lm.getName()].push_back(radius);
+  }
 }
 
 collision_detection::RobotSphereRepresentation::~RobotSphereRepresentation()
