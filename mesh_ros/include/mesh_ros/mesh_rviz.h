854c77154c043ec54901c375576ad1138e2bac47
Acorn <acorn@willowgarage.com>
2013-06-25 18:18:23 -0700

    mesh_ros: add RvizMeshShape to display mesh_core::Mesh

---
+++ mesh_ros/include/mesh_ros/mesh_rviz.h
@@ -0,0 +1,101 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Acorn Pooley */
+
+#ifndef MESH_ROS__MESH_RVIZ
+#define MESH_ROS__MESH_RVIZ
+
+#include <OGRE/OgreMaterial.h>
+#include <Eigen/Geometry>
+
+namespace mesh_core
+{
+  class Mesh;
+}
+
+namespace rviz
+{
+  class DisplayContext;
+}
+
+namespace Ogre
+{
+  class SceneNode;
+  class Vector3;
+  class Quaternion;
+  class ManualObject;
+}
+
+namespace mesh_ros
+{
+
+class RvizMeshShape
+{
+public:
+  RvizMeshShape(rviz::DisplayContext* context,
+                Ogre::SceneNode* parent_node,
+                const mesh_core::Mesh* mesh = NULL,
+                const Eigen::Vector4f& color = Eigen::Vector4f(1,1,1,1));
+
+  ~RvizMeshShape();
+
+  // reset to a new (or NULL) shape.
+  void reset(const mesh_core::Mesh* mesh = NULL);
+
+  // set color and alpha
+  void setColor(const Eigen::Vector4f& color);
+
+  virtual void setPosition(const Ogre::Vector3& position);
+  virtual void setOrientation(const Ogre::Quaternion& orientation);
+  const Ogre::Vector3& getPosition();
+  const Ogre::Quaternion& getOrientation();
+
+private:
+  Ogre::ManualObject* manual_object_;
+  Ogre::MaterialPtr material_;
+  std::string material_name_;
+
+  rviz::DisplayContext* context_;
+  Ogre::SceneNode* scene_node_;
+
+  // TODO: is this needed?  If not delete it.
+  //boost::shared_ptr<MarkerSelectionHandler> handler_;
+};
+
+}
+
+
+#endif
+
---
+++ mesh_ros/src/mesh_rviz.cpp
@@ -0,0 +1,166 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Acorn Pooley */
+
+#include <mesh_ros/mesh_rviz.h>
+#include <mesh_core/mesh.h>
+#include <console_bridge/console.h>
+
+//#include <rviz/default_plugin/markers/marker_selection_handler.h>
+//#include <rviz/default_plugin/marker_display.h>
+//#include <rviz/selection/selection_manager.h>
+
+#include <rviz/display_context.h>
+//#include <rviz/mesh_loader.h>
+//#include <rviz/default_plugin/marker_display.h>
+
+#include <OGRE/OgreSceneNode.h>
+#include <OGRE/OgreSceneManager.h>
+#include <OGRE/OgreManualObject.h>
+#include <OGRE/OgreMaterialManager.h>
+#include <OGRE/OgreTextureManager.h>
+#include <OGRE/OgreTechnique.h>
+
+#include <sstream>
+
+
+mesh_ros::RvizMeshShape::RvizMeshShape(
+      rviz::DisplayContext* context,
+      Ogre::SceneNode* parent_node,
+      const mesh_core::Mesh* mesh,
+      const Eigen::Vector4f& color)
+  : context_(context)
+  , scene_node_(parent_node->createChildSceneNode())
+{
+  static uint32_t count = 0;
+  std::stringstream ss;
+  ss << "RvizMeshShape" << count++;
+  manual_object_ = context_->getSceneManager()->createManualObject(ss.str());
+  scene_node_->attachObject(manual_object_);
+
+  ss << "Material";
+  material_name_ = ss.str();
+  material_ = Ogre::MaterialManager::getSingleton().create( material_name_, "rviz" );
+  material_->setReceiveShadows(false);
+  material_->getTechnique(0)->setLightingEnabled(true);
+  material_->setCullingMode(Ogre::CULL_NONE);
+
+  //handler_.reset( new MarkerSelectionHandler( this, MarkerID( new_message->ns, new_message->id ), context_ ));
+
+  setColor(color);
+
+  reset(mesh);
+}
+
+void mesh_ros::RvizMeshShape::setColor(
+      const Eigen::Vector4f& color)
+{
+  material_->getTechnique(0)->setLightingEnabled(true);
+  float r = color.x();
+  float g = color.y();
+  float b = color.z();
+  float a = color.w();
+  material_->getTechnique(0)->setAmbient( r,g,b );
+  material_->getTechnique(0)->setDiffuse( 0,0,0,a );
+
+  if (a < 0.9998)
+  {
+    material_->getTechnique(0)->setSceneBlending( Ogre::SBT_TRANSPARENT_ALPHA );
+    material_->getTechnique(0)->setDepthWriteEnabled( false );
+  }
+  else
+  {
+    material_->getTechnique(0)->setSceneBlending( Ogre::SBT_REPLACE );
+    material_->getTechnique(0)->setDepthWriteEnabled( true );
+  }
+}
+
+
+mesh_ros::RvizMeshShape::~RvizMeshShape()
+{
+  context_->getSceneManager()->destroyManualObject(manual_object_);
+  material_->unload();
+  Ogre::MaterialManager::getSingleton().remove(material_->getName());
+  context_->getSceneManager()->destroySceneNode(scene_node_);
+}
+
+void mesh_ros::RvizMeshShape::reset(
+      const mesh_core::Mesh* mesh)
+{
+  manual_object_->clear();
+
+  if (!mesh)
+    return;
+
+  manual_object_->estimateVertexCount(mesh->getTriCount() * 3);
+  manual_object_->begin(material_name_, Ogre::RenderOperation::OT_TRIANGLE_LIST);
+
+  std::vector<mesh_core::Mesh::Triangle>::const_iterator it = mesh->getTris().begin();
+  std::vector<mesh_core::Mesh::Triangle>::const_iterator end = mesh->getTris().end();
+  for ( ; it != end ; ++it)
+  {
+    for (int i = 0 ; i < 3 ; ++i)
+    {
+      const Eigen::Vector3d& v = mesh->getVert(it->verts_[i]);
+      manual_object_->position(v.x(), v.y(), v.z());
+    }
+  }
+
+  manual_object_->end();
+
+  //handler_->addTrackedObject( manual_object_ );
+}
+
+void mesh_ros::RvizMeshShape::setPosition( const Ogre::Vector3& position )
+{
+  scene_node_->setPosition( position );
+}
+
+void mesh_ros::RvizMeshShape::setOrientation( const Ogre::Quaternion& orientation )
+{
+  scene_node_->setOrientation( orientation );
+}
+
+const Ogre::Vector3& mesh_ros::RvizMeshShape::getPosition()
+{
+  return scene_node_->getPosition();
+}
+
+const Ogre::Quaternion& mesh_ros::RvizMeshShape::getOrientation()
+{
+  return scene_node_->getOrientation();
+}
+
+
