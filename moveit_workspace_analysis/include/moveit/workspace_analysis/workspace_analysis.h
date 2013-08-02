47a49293b02b8287530b5975af32876874078221
Sachin Chitta <sachinc@willowgarage.com>
2013-08-02 10:59:37 -0700

    changing to BSD license

--- moveit_workspace_analysis/include/moveit/workspace_analysis/workspace_analysis.h
+++ moveit_workspace_analysis/include/moveit/workspace_analysis/workspace_analysis.h
@@ -1,13 +1,22 @@
 /*********************************************************************
-*
-* Software License Agreement (All Rights Reserved)
+* Software License Agreement (BSD License)
 *
 *  Copyright (c) 2013, Willow Garage, Inc.
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
-*  modification is not permitted without the explicit permission of
-*  Willow Garage Inc.
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
 *
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -22,7 +31,7 @@
 *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 *  POSSIBILITY OF SUCH DAMAGE.
 *
-* Author: Sachin Chitta
+*  Author: Sachin Chitta
 *********************************************************************/
 
 #ifndef MOVEIT_WORKSPACE_ANALYSIS_H_
